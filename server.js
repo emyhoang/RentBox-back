3d5d4f0801f8e9c5cafa611d9fefa2af23f71c56
emyhoang <emyhoang8899@gmail.com>
2019-02-18 23:02:21 -0800

    allow post, put, delete on cors

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(flash());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
   next();
 })
 
