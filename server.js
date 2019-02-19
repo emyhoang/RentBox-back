794995207b236a43354f4eb9e5a36634fe0483af
emyhoang <emyhoang8899@gmail.com>
2019-02-18 23:01:18 -0800

    fix cors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(session({
 app.use(flash());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
 
